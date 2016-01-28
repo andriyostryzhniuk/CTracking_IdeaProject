a8a9a0c891fbea430f8977d5a095e1bfa602020d
andriyostryzhniuk <ostryzhnykandriy@gmail.com>
2016-01-28 14:27:39 +0200

    DtoEmploeesFullName

---
+++ src/main/java/sample/dto/DtoEmploeesFullName.java
@@ -0,0 +1,40 @@
+package sample.dto;
+
+/**
+ * Created by Andriy on 01/28/2016.
+ */
+public class DtoEmploeesFullName {
+    private String name;
+    private String surname;
+    private String middleName;
+
+    public DtoEmploeesFullName(String name, String surname, String middleName) {
+        this.name = name;
+        this.surname = surname;
+        this.middleName = middleName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+}
