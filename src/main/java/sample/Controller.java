7bce8a55b681a5983677843cefc2d31ff3fc5c8a
andriyostryzhniuk <ostryzhnykandriy@gmail.com>
2016-01-30 13:49:26 +0200

    created the first dinamic window

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,33 +1,29 @@
 package sample;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.GridPane;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
+import java.io.IOException;
 
 public class Controller {
 
     public GridPane mainGridPane;
 
-    public void init(ActionEvent actionEvent) {
-        ObservableList<String> usersData = FXCollections.observableArrayList();
-        usersData.add("yjryryhrty");
-        usersData.add("tytyujryuryu");
-        ListView listView = new ListView(usersData);
-        mainGridPane.add(listView, 2, 2);
+    public void createEditTableTracking(ActionEvent actionEvent) throws IOException {
 
-        DateFormat dayFormat = new SimpleDateFormat("EEEE", new Locale("uk"));
-        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yy");
-        Date date = new Date();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/employeesWorkTracking.fxml"));
+        try {
+            mainGridPane.add(fxmlLoader.load(), 1, 1);
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
 
-        mainGridPane.add(new Label(dayFormat.format(date).toString().substring(0, 1).toUpperCase()+
-                dayFormat.format(date).toString().substring(1)+"\n"+
-                dateFormat.format(date)), 3, 1);
+//        DateFormat dayFormat = new SimpleDateFormat("EEEE", new Locale("uk"));
+//        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yy");
+//        Date date = new Date();
+//
+//        mainGridPane.add(new Label(dayFormat.format(date).toString().substring(0, 1).toUpperCase()+
+//                dayFormat.format(date).toString().substring(1)+"\n"+
+//                dateFormat.format(date)), 3, 1);
     }
 }
