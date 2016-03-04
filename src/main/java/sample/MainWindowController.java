0aa3c1e35c08e1eacb24ea28805615c04d7f7c8e
andriyostryzhniuk <ostryzhnykandriy@gmail.com>
2016-03-04 18:24:10 +0200

    created ButtonClose

--- src/main/java/sample/MainWindowController.java
+++ src/main/java/sample/MainWindowController.java
@@ -2,7 +2,17 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
 import javafx.scene.layout.GridPane;
+import javafx.util.Callback;
+
 import java.io.IOException;
 import java.io.UncheckedIOException;
 
@@ -17,5 +27,19 @@ public class MainWindowController {
         } catch (IOException exception) {
             throw new UncheckedIOException(exception);
         }
+        initButtonClose();
+    }
+
+    public void initButtonClose (){
+        Button buttonClose = new Button("Закрити");
+        buttonClose.setPrefHeight(25.0);
+        buttonClose.setPrefWidth(85.0);
+        buttonClose.setOnAction((ActionEvent event) -> {
+            mainGridPane.getChildren().remove(1, 3);
+        });
+        mainGridPane.add(buttonClose, 1, 2);
+        mainGridPane.setHalignment(buttonClose, HPos.RIGHT);
+        mainGridPane.setValignment(buttonClose, VPos.TOP);
+        mainGridPane.setMargin(buttonClose, new Insets(20, 0, 0, 0));
     }
 }
