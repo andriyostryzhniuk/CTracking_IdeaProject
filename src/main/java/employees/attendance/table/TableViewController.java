51ac42e8bd4946f3fcc32a265613301a90f7f0c2
andriyostryzhniuk <ostryzhnykandriy@gmail.com>
2016-03-07 14:51:59 +0200

    created adding and removing TextFiled

--- src/main/java/employees/attendance/table/TableViewController.java
+++ src/main/java/employees/attendance/table/TableViewController.java
@@ -4,13 +4,16 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.Cursor;
+import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import dto.DtoEmployeesFullName;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
 import javafx.util.Callback;
 import sample.ODBC_PubsBD;
 
@@ -83,7 +86,7 @@ public class TableViewController<T extends DtoEmployeesFullName> {
 
         tableView.setStyle("-fx-accent: derive(-fx-control-inner-background, -20%); -fx-control-inner-background, -80%;");
         tableView.getTableView().setFixedCellSize(40.0);
-        tableView.getTableView().getStylesheets().add(getClass().getResource("/employees.attendance.table/ColumnDisableStyle.css").toExternalForm());
+        tableView.getTableView().getStylesheets().add(getClass().getResource("/employees.attendance.table/TableViewStyle.css").toExternalForm());
 
         colName.setPercentWidth(150.0);
         colName.setMinWidth(150.0);
@@ -139,12 +142,28 @@ public class TableViewController<T extends DtoEmployeesFullName> {
                         final TableCell<T, String> cell = new TableCell<T, String>() {
                             @Override
                             public void updateItem(String item, boolean empty) {
+                                GridPane gridPane = new GridPane();
+                                TextField textField = new TextField();
+
+                                Region region = new Region();
+                                region.setStyle("-fx-background-color: red");
+//                                gridPane.add(region, 0, 0);
+
+                                ColumnConstraints columnConstraints = new ColumnConstraints();
+                                columnConstraints.setMinWidth(38.0);
+                                gridPane.getColumnConstraints().add(columnConstraints);
+
+                                RowConstraints rowConstraints = new RowConstraints();
+                                rowConstraints.setMinHeight(38.0);
+                                gridPane.getRowConstraints().add(rowConstraints);
+
                                 CheckBox checkBox = new CheckBox();
                                 checkBox.getStylesheets().add(getClass().getResource("/CheckBoxStyle.css").toExternalForm());
                                 checkBox.setCursor(Cursor.HAND);
                                 if (param.getStyleClass().indexOf("disable") != -1) {
                                     checkBox.setDisable(true);
                                 }
+                                gridPane.add(checkBox, 0, 0);
 
                                 super.updateItem(item, empty);
                                 if (empty) {
@@ -154,8 +173,28 @@ public class TableViewController<T extends DtoEmployeesFullName> {
                                     checkBox.setOnAction((ActionEvent event) -> {
                                         System.out.println(param.getId());
                                         Callback<TableColumn<T, String>, TableCell<T, String>> cellFactory1 = param.getCellFactory();
+
+                                        if (checkBox.isSelected() == true) {
+                                            textField.setPrefHeight(16.0);
+                                            textField.setMinHeight(16.0);
+                                            textField.setAlignment(Pos.CENTER);
+                                            textField.setPadding(Insets.EMPTY);
+                                            gridPane.add(textField, 0, 0);
+                                            gridPane.setHalignment(textField, HPos.CENTER);
+                                            gridPane.setValignment(textField, VPos.BOTTOM);
+                                        } else {
+                                            ObservableList<Node> childrens = gridPane.getChildren();
+                                            int i = 0;
+                                            for (Node N : childrens) {
+                                                if (textField.equals(N)) {
+                                                    childrens.remove(i);
+                                                    break;
+                                                }
+                                                i++;
+                                            }
+                                        }
                                     });
-                                    setGraphic(checkBox);
+                                    setGraphic(gridPane);
                                     setText(null);
                                 }
                             }
@@ -166,42 +205,4 @@ public class TableViewController<T extends DtoEmployeesFullName> {
         return cellFactory;
     }
 
-    public Callback<TableColumn<T, String>, TableCell<T, String>> textFieldCellFactory() {
-        Callback<TableColumn<T, String>, TableCell<T, String>> cellFactory =
-                new Callback<TableColumn<T, String>, TableCell<T, String>>() {
-                    @Override
-                    public TableCell call(final TableColumn<T, String> param) {
-
-                        final TableCell<T, String> cell = new TableCell<T, String>() {
-
-                            @Override
-                            public void updateItem(String item, boolean empty) {
-
-                                super.updateItem(item, empty);
-                                if (empty) {
-                                    setGraphic(null);
-                                    setText(null);
-                                } else {
-                                    TextField textField = new TextField();
-                                    try {
-                                        textField.setText(Integer.toString(ODBC_PubsBD.selectDefaultEmployeesWorkingHours(
-                                                1)));
-                                        //calculateEmployeesId(checkBoxId))));
-                                    } catch (NullPointerException e) {
-                                        e.printStackTrace();
-                                    }
-
-                                    textField.setPrefHeight(16.0);
-                                    textField.setMinHeight(16.0);
-                                    textField.setAlignment(Pos.CENTER);
-                                    textField.setPadding(Insets.EMPTY);
-                                    setGraphic(textField);
-                                }
-                            }
-                        };
-                        return cell;
-                    }
-                };
-        return cellFactory;
-    }
 }
---
+++ src/main/java/employees/attendance/table/windowAttendanceController.java
@@ -0,0 +1,140 @@
+package employees.attendance.table;
+
+import dto.DtoEmployeesFullName;
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import sample.ODBC_PubsBD;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Created by Andriy on 01/30/2016.
+ */
+public class WindowAttendanceController<T extends DtoEmployeesFullName> {
+
+    @FXML
+    public BorderPane rootBorderPane;
+
+//    @FXML
+//    private Controller2 controller2;
+
+    public date.picker.DatePicker datePicker;
+
+    private int previousMonth;
+    private int previousYear;
+    private TableViewController tableViewController;
+
+    @FXML
+    private void initialize() {
+
+        initDatePicker();
+
+//        System.out.println(getDatePickerValue());
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/employees.attendance.table/TableView.fxml"));
+        try {
+            rootBorderPane.setCenter(fxmlLoader.load());
+            tableViewController = fxmlLoader.getController();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+        updateEmployeesFullNameList();
+    }
+
+    public void initDatePicker() {
+        datePicker = new date.picker.DatePicker();
+
+        datePicker.setLocale(new Locale("uk"));
+        datePicker.getCalendarView().setShowWeeks(false);
+
+        datePicker.setSelectedDate(new Date());
+        datePicker.deselectSelection();
+
+        previousMonth = datePicker.selectedDateProperty().get().getMonth();
+        previousYear = datePicker.selectedDateProperty().get().getYear();
+
+        datePicker.selectedDateProperty().addListener(new InvalidationListener() {
+            @Override
+            public void invalidated(Observable observable) {
+                boolean isException = false;
+                DateFormat dateFormat = new SimpleDateFormat("yyyy-MM");
+                try {
+                    dateFormat.format(datePicker.selectedDateProperty().get());
+                } catch (NullPointerException e) {
+                    isException = true;
+                }
+                if (isException == false && previousMonth != datePicker.selectedDateProperty().get().getMonth() ||
+                        isException == false && previousYear != datePicker.selectedDateProperty().get().getYear()) {
+
+                    updateEmployeesFullNameList ();
+
+                }
+            }
+        });
+
+        rootBorderPane.setTop(datePicker);
+        rootBorderPane.setAlignment(rootBorderPane, Pos.TOP_LEFT);
+        rootBorderPane.setMargin(datePicker, new Insets(0.0, 0.0, 20.0, 0.0));
+    }
+
+    public void updateEmployeesFullNameList () {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(datePicker.selectedDateProperty().get());
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+        int daysOfMonthNumber = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+        String firstDayOfMonth = dateFormat.format(calendar.getTime());
+        calendar.set(Calendar.DAY_OF_MONTH, daysOfMonthNumber);
+        String lastDayOfMonth = dateFormat.format(calendar.getTime());
+
+        tableViewController.getEmployeesFullNameList().clear();
+        tableViewController.getEmployeesFullNameList().addAll(ODBC_PubsBD.selectEmployeesFullName(firstDayOfMonth, lastDayOfMonth));
+
+        updateDateInTableHeader (calendar);
+
+        previousMonth = datePicker.selectedDateProperty().get().getMonth();
+        previousYear = datePicker.selectedDateProperty().get().getYear();
+    }
+
+    public void updateDateInTableHeader (Calendar calendar) {
+//        tableViewController.getCalendar().setTime(datePicker.selectedDateProperty().get());
+        DateFormat dateFormatForText = new SimpleDateFormat("dd.MM");
+        DateFormat dateFormatForId = new SimpleDateFormat("yyyy-MM-dd");
+
+        ObservableList<TableColumn<T, ?>> tableColumns = tableViewController.getTableView().getColumns();
+        int i = 0;
+
+        int thisMonth = calendar.get(Calendar.MONTH);
+
+        Calendar dayIncrement = calendar;
+        dayIncrement.set(Calendar.DAY_OF_MONTH, 1);
+
+        for (TableColumn<T, ?> C: tableColumns) {
+            if (i > 0) {
+                C.getStyleClass().remove("disable");
+                C.setText(dateFormatForText.format(dayIncrement.getTime()));
+                C.setId(dateFormatForId.format(dayIncrement.getTime()));
+                if (dayIncrement.get(Calendar.MONTH) != thisMonth) {
+                    C.getStyleClass().add("disable");
+                }
+                dayIncrement.add(Calendar.DAY_OF_MONTH, 1);
+            }
+            i++;
+        }
+    }
+
+}
